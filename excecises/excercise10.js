c0450d89a63bb7e4560f3d415f4246956b7592dd
MaUxD0 <kevinmauriciob3@gmail.com>
2024-09-13 17:14:44 -0500

    Resuelto ejercicio 9

--- excecises/excercise10.js
+++ excecises/excercise10.js
@@ -1,15 +1,14 @@
-/*
-Ejercicio 10: Calculadora de años bisiestos
-Escribe una función llamada esBisiesto(anio) 
-que reciba un año como argumento y retorne "Es bisiesto" si el año es divisible entre 4 pero no entre 100, 
-o si es divisible entre 400. En caso contrario, retorne "No es bisiesto".
-*/
 function esBisiesto(anio) {
-    // TODO: Resolver
+    // Verificamos si el año es bisiesto
+    if ((anio % 4 === 0 && anio % 100 !== 0) || (anio % 400 === 0)) {
+        return "Es bisiesto";
+    } else {
+        return "No es bisiesto";
+    }
 }
 
-console.log("-----------------------------------")
-console.log("EJERCICIO 10")
-console.log("-----------------------------------")
+console.log("-----------------------------------");
+console.log("EJERCICIO 10");
+console.log("-----------------------------------");
 console.log(esBisiesto(2020)); // "Es bisiesto"
 console.log(esBisiesto(2100)); // "No es bisiesto"
