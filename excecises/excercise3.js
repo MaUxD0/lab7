107410e493d26946fa1813b3c939e61143a6cebf
MaUxD0 <kevinmauriciob3@gmail.com>
2024-09-13 16:49:39 -0500

    Resuelto ejercicio 3

--- excecises/excercise3.js
+++ excecises/excercise3.js
@@ -1,15 +1,14 @@
-/*
-Ejercicio 3: Mayor de dos números
-Crea una función llamada mayorDeDos(num1, num2)
-que reciba dos números como argumentos y retorne el mayor de los dos. 
-Si los números son iguales, debe retornar "Los números son iguales".
-*/
 function mayorDeDos(num1, num2) {
-    // TODO: Resolver
+    // Verificamos si los números son iguales
+    if (num1 === num2) {
+        return "Los números son iguales";
+    }
+    // Retornamos el mayor de los dos números
+    return num1 > num2 ? num1 : num2;
 }
 
-console.log("-----------------------------------")
-console.log("EJERCICIO 3")
-console.log("-----------------------------------")
+console.log("-----------------------------------");
+console.log("EJERCICIO 3");
+console.log("-----------------------------------");
 console.log(mayorDeDos(5, 10)); // 10
 console.log(mayorDeDos(8, 8));  // "Los números son iguales"
