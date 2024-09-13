d646d3b60d9fc4fdc8c20c7ff916e52c44977333
MaUxD0 <kevinmauriciob3@gmail.com>
2024-09-13 17:04:16 -0500

    Resuelto ejercicio 6

--- excecises/excercise6.js
+++ excecises/excercise6.js
@@ -1,16 +1,18 @@
-/*
-Ejercicio 6: Calculador de descuento
-Crea una función llamada calcularDescuento(precio, porcentaje) 
-que reciba el precio de un producto y un porcentaje de descuento, 
-y retorne el precio final con el descuento aplicado. 
-Si el porcentaje es mayor a 100 o menor que 0, debe retornar "Porcentaje no válido".
-*/
 function calcularDescuento(precio, porcentaje) {
-    // TODO: Resolver
+    // Verificamos si el porcentaje es válido
+    if (porcentaje < 0 || porcentaje > 100) {
+        return "Porcentaje no válido";
+    }
+    
+    // Calculamos el descuento y el precio final
+    const descuento = (precio * porcentaje) / 100;
+    const precioFinal = precio - descuento;
+    
+    return precioFinal;
 }
 
-console.log("-----------------------------------")
-console.log("EJERCICIO 6")
-console.log("-----------------------------------")
+console.log("-----------------------------------");
+console.log("EJERCICIO 6");
+console.log("-----------------------------------");
 console.log(calcularDescuento(100, 20)); // 80
 console.log(calcularDescuento(50, -5));  // "Porcentaje no válido"
